a31bee6d86d91689065c79f1c735b30c9e4fa7c7
Cathy McDonald <cthmcdnld@gmail.com>
2017-05-20 14:08:01 +1000

    Format code

--- module5-assignment/js/script.js
+++ module5-assignment/js/script.js
@@ -80,16 +80,13 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
-       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 
     		  											"randomCategoryShortName", 
     		  											"'" + chosenCategoryShortName + "'");
-
-
-       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-
+      
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
